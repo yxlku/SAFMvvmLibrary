25a5352361c7c7230864f6b28298dcaf253978b5
longpc <1379035808@qq.com>
2021-01-19 20:08:39 +0800

    1、任务提醒功能；

---
+++ modules/ModuleDesigner/src/main/java/com/deti/designer/order/popup/FilterOrderPopupView.kt
@@ -0,0 +1,13 @@
+package com.deti.designer.order.popup
+
+import android.app.Activity
+import com.lxj.xpopup.core.BottomPopupView
+
+class FilterOrderPopupView(
+    var mActivity: Activity
+): BottomPopupView(mActivity) {
+
+
+//    override fun getImplLayoutId(): Int =
+
+}
